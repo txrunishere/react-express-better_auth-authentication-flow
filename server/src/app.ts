aaae20abe28d444146919f378655a1c44457af24
txrunishere <tsoni9286@gmail.com>
2026-03-16 14:08:49 +0530

    feat: add authentication routes and user retrieval functionality

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,9 @@
 import express, { type Response } from "express";
 import cors from "cors";
 import config from "./config";
+import { prisma } from "./utils/prisma";
+import { fromNodeHeaders, toNodeHandler } from "better-auth/node";
+import { auth } from "./utils/auth";
 
 const app = express();
 
@@ -12,11 +15,35 @@ app.use(
   }),
 );
 
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
 app.use(express.json());
 
 app.get("/", (_, res: Response) => {
   return res.json({
     hello: "world",
+  });
+});
+
+app.get("/api/me", async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  });
+
+  if (!session) {
+    return res.json({
+      error: "Unauthorized",
+    });
+  }
+
+  return res.json(session);
+});
+
+app.get("/users", async (_, res: Response) => {
+  const users = await prisma.user.findMany();
+
+  return res.json({
+    users,
   });
 });
 
